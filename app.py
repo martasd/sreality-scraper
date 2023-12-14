3a408f836c83c4772b623754e09653c8b4036cfb
Martin Dluhoš <dluhos.martin@gmail.com>
2023-12-14 21:22:30 +0100

    Create the Flat model

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+# TODO:
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@localhost/dbname"
+
+
+class Flat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    url = db.Column(db.String(), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "<Flat %r>" % self.title
 
 
 @app.route("/")
 def index():
-    return "TODO"
+    flats = Flat.query.all()
+    return render_template("index.html", flats=flats)
