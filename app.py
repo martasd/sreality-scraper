f8c8ec8a5e3a23002a00e9dc6cadb527c19164f0
Martin Dluhoš <dluhos.martin@gmail.com>
2023-12-19 11:29:26 +0100

    Seed the database

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import requests
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from os import environ
+from requests import JSONDecodeError
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL")
@@ -21,6 +24,26 @@ class Flat(db.Model):
 
     def __repr__(self):
         return "<Flat %r>" % self.title
+
+
+@app.cli.command("seed_db")
+def seed_db():
+    url = "https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&per_page=500"
+    res = requests.get(url)
+    try:
+        res_json = res.json()
+
+        for estate in res_json["_embedded"]["estates"]:
+            flat = Flat(
+                title=estate["name"],
+                location=estate["locality"],
+                image_url=estate["_links"]["images"][0]["href"],
+            )
+
+            db.session.add(flat)
+            db.session.commit()
+    except JSONDecodeError:
+        print("The response is not a valid JSON.")
 
 
 @app.route("/")
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,6 @@ set -o nounset
 
 flask db upgrade
 
+flask seed_db
+
 flask run --host=0.0.0.0 --port=8080
